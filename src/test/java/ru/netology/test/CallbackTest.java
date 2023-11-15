66c2453073e37d0cb68334815240fe751467a69d
Aleksey <Lesha55-90@mail.ru>
2023-11-15 06:27:11 +0600

    ChangeCode2

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -22,18 +22,18 @@ public class CallbackTest {
     }
 
     @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
+    public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
+
     }
 
     @AfterEach
-    void tearDown() {
+    public void afterEach() {
         driver.quit();
         driver = null;
     }
@@ -44,8 +44,7 @@ public class CallbackTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79000000000");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        String text = driver.findElement(By.cssSelector("data-test-id=order-success")).getText();
-        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", driver.findElement(By.cssSelector("data-test-id=order-success")).getText());
 
     }
 
